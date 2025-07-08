383d6cef52cf3e07832d7c3b46f51180626c0572
SemTex-H <oyunchuluunganbold@gmail.com>
2025-07-08 11:05:34 +0000

    add: config/, launch/, script/

---
+++ script/cam_test.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import cv2
+
+class CameraSubscriber(Node):
+    def __init__(self):
+        super().__init__('camera_subscriber')
+        self.subscription = self.create_subscription(
+            Image,
+            '/fastbot_1/camera/image_raw',
+            self.listener_callback,
+            10)
+        self.cv_bridge = CvBridge()
+
+    def listener_callback(self, msg):
+        self.get_logger().info('Image received')
+        try:
+            cv_image = self.cv_bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+            cv2.imshow('Camera Feed', cv_image)
+            cv2.waitKey(1)
+        except Exception as e:
+            self.get_logger().error(f"Image conversion failed: {e}")
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = CameraSubscriber()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        cv2.destroyAllWindows()
+        node.destroy_node()
+        rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
